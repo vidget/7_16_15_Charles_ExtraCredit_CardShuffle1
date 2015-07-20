977faa3a3f916d46efb00deef80b4e7b320775ca
Vidget <vidget@yahoo.com>
2015-07-20 09:51:29 -0400

    Card Shuffle
    Draws a selected number of cards from a deck of 52 cards.

---
+++ 7_16_15_Charles_ExtraCredit_CardShuffle1/Hand.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_16_15_ExperIT_Charles_ExtraCredit_1
+{
+    //Gregory Scott Norris - ExperienceIT 7-16-15
+    //Hand Class, holds cards and parameters for 5 cards
+
+    class Hand
+    {
+        //currently not needed for single user, but will use when multiplayer
+        public string card1 { get; set; }
+        public string card2 { get; set; }
+        public string card3 { get; set; }
+        public string card4 { get; set; }
+        public string card5 { get; set; }
+
+        public int cardValue1 { get; set; }
+        public int cardValue2 { get; set; }
+        public int cardValue3 { get; set; }
+        public int cardValue4 { get; set; }
+        public int cardValue5 { get; set; }
+
+        //creates a dictionary that holds 52 card strings with unicode card symbol \u0003 = Hearts \u0004 = Diamonds \u0005 = Clubs \u0006 = Spades
+        Dictionary<int, string> cards = new Dictionary<int, string>()
+            { 
+                { 0,"A\u0006"},
+                { 1,"2\u0006"},
+                { 2,"3\u0006"},
+                { 3,"4\u0006"},
+                { 4,"5\u0006"},
+                { 5,"6\u0006"},
+                { 6,"7\u0006"},
+                { 7,"8\u0006"},
+                { 8,"9\u0006"},
+                { 9,"10\u0006"},
+                { 10,"J\u0006"},
+                { 11,"Q\u0006"},
+                { 12,"K\u0006"},
+                { 13,"A\u0005"},
+                { 14,"2\u0005"},
+                { 15,"3\u0005"},
+                { 16,"4\u0005"},
+                { 17,"5\u0005"},
+                { 18,"6\u0005"},
+                { 19,"7\u0005"},
+                { 20,"8\u0005"},
+                { 21,"9\u0005"},
+                { 22,"10\u0005"},
+                { 23,"J\u0005"},
+                { 24,"Q\u0005"},
+                { 25,"K\u0005"},
+                { 26,"A\u0003"},
+                { 27,"2\u0003"},
+                { 28,"3\u0003"},
+                { 29,"4\u0003"},
+                { 30,"5\u0003"},
+                { 31,"6\u0003"},
+                { 32,"7\u0003"},
+                { 33,"8\u0003"},
+                { 34,"9\u0003"},
+                { 35,"10\u0003"},
+                { 36,"J\u0003"},
+                { 37,"Q\u0003"},
+                { 38,"K\u0003"},
+                { 39,"A\u0004"},
+                { 40,"2\u0004"},
+                { 41,"3\u0004"},
+                { 42,"4\u0004"},
+                { 43,"5\u0004"},
+                { 44,"6\u0004"},
+                { 45,"7\u0004"},
+                { 46,"8\u0004"},
+                { 47,"9\u0004"},
+                { 48,"10\u0004"},
+                { 49,"J\u0004"},
+                { 50,"Q\u0004"},
+                { 51,"K\u0004"},
+            };
+
+        public Hand()
+        {
+            card1 = "";
+            card2 = "";
+            card3 = "";
+            card4 = "";
+            card5 = "";
+
+            cardValue1 = 0;
+            cardValue2 = 0;
+            cardValue3 = 0;
+            cardValue4 = 0;
+            cardValue5 = 0;
+        }
+
+
+        public void addCardtoHand(int cardNumber, List<string> userHand)
+        {
+            userHand.Add(cards[cardNumber]);
+        }
+
+
+
+        public int drawCard()
+        {
+            //generates a random number between 0-51
+            Random random = new Random();
+            int randomNumber = random.Next(0, 52);
+            return randomNumber;
+
+        }
+
+        public void removeCard(int[] deck, int userCard)//removes the card the user drew from the deck by setting the value of the deck array from 1 to 0
+        {
+            deck[userCard] = 0;
+        }
+
+        public void displayHand(List<string> userHand)//displays all the strings stored in the userHand list
+        {
+            Console.WriteLine("\n Your Hand:\n");
+
+            for (int i = 0; i < userHand.Count; i++)
+            {
+                Console.Write(" " + userHand[i]);
+            }
+            Console.WriteLine();
+
+
+        }
+
+        public bool checkIfAvailableCard(int[] deck, int tempCard)//checks to see if the deck array contains a 1 or 0 taken or not, returns a BOOL
+        {
+            if (deck[tempCard] == 1)
+                return false;//card is taken
+            else
+                return true;// not taken
+        }
+
+    }
+}
---
+++ 7_16_15_Charles_ExtraCredit_CardShuffle1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Gregory Scott Norris - ExperienceIT 7-16-15
+//Exercise 1: Shuffle Cards
+//Write a program to create a standard 52 card deck and then deal a random hand to a player. Once a card is dealt then it can't be used again.
+
+
+namespace _7_16_15_ExperIT_Charles_ExtraCredit_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a string array that holds a deck of 52 cards 1 = card in deck, 0 = card drawn
+            int[] deck = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            List<string> userHand = new List<string>();//userhand holds the string of the card number & suit drawn by the user
+            List<Hand> gameUser = new List<Hand>();//not really used in this program, but will be used in the multiplayer version. 
+
+            int tempCard;//holds the drawn card
+            int numberOfDraws = 0;//user input number of cards to draw
+            bool isCardGone = true;//determines if a card is available or already taken from the deck
+
+            Hand hand1 = new Hand();//creates a HAND class called hand1 and puts it into the LIST GAMEUSER
+            gameUser.Add(hand1);//using a list isn't necessary here, but will be when I modify for multiplayer
+
+
+            Console.WriteLine("\n How many cards would you like to draw?");//prompts user for number of cards to draw
+            Console.Write(" >");//user prompt
+
+            numberOfDraws = int.Parse(Console.ReadLine());//parses in the user input for number of draws
+            Console.Clear();
+
+
+            for (int i = 0; i < numberOfDraws; i++)//loops through the selecting cards, adding to hand and removing from the deck
+            {
+
+                do//This will loop until the random number generates an available card.
+                {
+                    tempCard = gameUser[0].drawCard(); //drawCard Method generates a random number between 1&52
+                    isCardGone = gameUser[0].checkIfAvailableCard(deck, tempCard);//sends deck & drawn card to method to check if card drawn is still in deck, returns true if card has already been drawn
+                } while (isCardGone == true); ////check that card hasn't been drawn yet
+
+                gameUser[0].addCardtoHand(tempCard, userHand);//add the card to the userHand
+                gameUser[0].removeCard(deck, tempCard);//remove card from the deck
+
+            }
+            gameUser[0].displayHand(userHand);//display the users hand.
+            Console.ReadLine();
+        }
+
+    }
+}
